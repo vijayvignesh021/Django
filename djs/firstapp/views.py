44c63e994bef1ec42709b1a19afa4bb55547d602
vijay vignesh <97654626+vijayvignesh021@users.noreply.github.com>
2022-09-27 19:38:06 +0530

    First_Django

---
+++ djs/firstapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+user={}
+temp = loader.get_template('firstapp/index.html')
+def num(request):
+    return HttpResponse("<h1>+91 8825792941</h1>")
+def inx(request):
+    return HttpResponse((loader.get_template('firstapp/fist.html')).render({},request))
+def login(request):
+    global user
+    if request.method=="POST":
+        print("values are",[x for x in request.POST])
+        username = request.POST['name']
+        password = request.POST['pass'] 
+        user[username] = password
+        print(user)
+        return  HttpResponse('<h1>Succes ..!</h1>')
+
+    else:
+        return  HttpResponse(temp.render({},request))
